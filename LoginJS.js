9df53c4c7398f7398a4850157819b589744cd2dc
Aman Sinha <47569617+developersway@users.noreply.github.com>
2019-05-18 19:43:28 +0530

    Delete LoginJS.js

--- LoginJS.js
+++
@@ -1,7 +0,0 @@
- document.getElementById('but').onclick=function(){
-        var username=document.getElementById('input1').value;
-        localStorage.setItem("textvalue",username);
-        alert("Hi "+username +"! Press OK to continue");
-        window.location="file:///C:/Users/AMAN/Desktop/Reaction/index1.html"    
-        
-    }
